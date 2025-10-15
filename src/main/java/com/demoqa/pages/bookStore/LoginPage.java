e9e52960ca46a20b5660422f5a9a29ffa2057d65
AlexAlimow <gorlum007rus@gmail.com>
2025-10-15 19:40:48 +0200

    CW15.10.2025

---
+++ src/main/java/com/demoqa/pages/bookStore/LoginPage.java
@@ -0,0 +1,34 @@
+package com.demoqa.pages.bookStore;
+
+import com.demoqa.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy(id = "userName")
+    WebElement userNameInput;
+
+    @FindBy(id = "password")
+    WebElement passwordInput;
+
+    public LoginPage enterUserData(String name, String password) {
+        type(userNameInput,name);
+        type(passwordInput,password);
+        return this;
+    }
+
+
+    @FindBy(id = "login")
+    WebElement loginButton;
+
+    public ProfilePage clickOnLiginButton() {
+        click(loginButton);
+        return new ProfilePage(driver);
+
+    }
+}
